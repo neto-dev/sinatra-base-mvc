4d7320454713b135c7e61a3cfd292661bf8eac96
Ernesto Valenzuela <hello@netov.dev>
2020-05-21 08:42:35 -0500

    Agregando estructura de Seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,20 @@
+# require_relative "../app/models/user"
+# 5.times do |i|
+#    User.create(username: "@neto_dev",
+#              first_name: "Ernesto",
+#              last_name: "Valenzuela",
+#              email: "hello@netov.dev",
+#              password: "neto",
+#              age: 26)
+# end
 
-5.times do |i|
-   User.create(username: "@neto_dev",
-             first_name: "Ernesto",
-             last_name: "Valenzuela",
-             email: "hello@netov.dev",
-             password: "neto",
-             age: 26)
+class User < ActiveRecord::Base
+      5.times do |i|
+         User.create(username: "@neto_dev",
+                     first_name: "Ernesto",
+                     last_name: "Valenzuela",
+                     email: "hello@netov.dev",
+                     password: "neto",
+                     age: 26)
+      end
 end
